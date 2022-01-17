d69dbddeb1cb473bf1eb094097f8dfe6e8085abc
burakkuhokuignite <matzsx@gmail.com>
2022-01-17 15:14:20 -0300

    operações básicas concluídas

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,34 @@
-
+import React, {useState, useEffect} from "react";
 import './App.css';
+import Axios from 'axios';
 
 function App() {
 
-  const{nomeLugar, setNomeLugar} = useState('')
-  const{descricaoLugar, setDescricaoLugar} = useState('')
-  const{enderecoLugar, setEnderecoLugar} = useState('')
-  const{estadoLugar, setEstadoLugar} = useState('')
-  const{cidadeLugar, setCidadeLugar} = useState('')
+  const[nomeLugar, setNomeLugar] = useState("")
+  const[descricaoLugar, setDescricaoLugar] = useState("")
+  const[enderecoLugar, setEnderecoLugar] = useState('')
+  const[estadoLugar, setEstadoLugar] = useState('')
+  const[cidadeLugar, setCidadeLugar] = useState('')
+
+  const[listaPontos, setListaPontos] = useState([])
+
+  useEffect(() => { //receber os pontos turisticos já cadastrados
+    Axios.get('http://localhost:3001/api/get').then((response) =>{
+      setListaPontos(response.data)
+    })
+  })
+
+  const submitLugar = () => { //enviar um novo ponto turistico
+    Axios.post("http://localhost:3001/api/insert", {nomeLugar: nomeLugar, descricaoLugar: descricaoLugar, enderecoLugar: enderecoLugar, estadoLugar: estadoLugar, cidadeLugar: cidadeLugar}).then(() => {
+      alert("Sucesso!");
+    })
+  };
+
+
+  const deleteLugar = (nomeL) => {
+
+    Axios.delete(`http://localhost:3001/api/delete/${nomeL}`);
+  }
 
 
   return (
@@ -16,17 +37,36 @@ function App() {
 
       <div className="form">
         <label>Nome do Ponto Turístico:</label>
-        <input type="text" name="nomeLugar"></input>
+        <input type="text" name="nomeLugar" onChange={(e) =>{
+          setNomeLugar(e.target.value);
+        }}></input>
         <label>Descrição:</label>
-        <input type="text" name="descricaoLugar"></input>
+        <input type="text" name="descricaoLugar" onChange={(e) =>{
+          setDescricaoLugar(e.target.value);
+        }}></input>
         <label>Endereço:</label>
-        <input type="text" name="enderecoLugar"></input>
+        <input type="text" name="enderecoLugar" onChange={(e) =>{
+          setEnderecoLugar(e.target.value);
+        }}></input>
         <label>Estado:</label>
-        <input type="text" name="estadoLugar"></input>
+        <input type="text" name="estadoLugar" onChange={(e) =>{
+          setEstadoLugar(e.target.value);
+        }}></input>
         <label>Cidade:</label>
-        <input type="text" name="cidadeLugar"></input>
+        <input type="text" name="cidadeLugar" onChange={(e) =>{
+          setCidadeLugar(e.target.value);
+        }}></input>
 
-        <button>Cadastrar</button>
+        <button onClick={submitLugar}>Cadastrar</button>
+
+        {listaPontos.map((val) => {
+          return <div className="card"> <h1> {val.nome}  </h1> <p> Descrição: {val.descricao} </p> <p> Endereço: {val.endereco} </p> <p> Estado: {val.estado} </p> <p> Cidade: {val.cidade} </p>
+            <button onClick={() => {deleteLugar(val.nome)}}>Deletar</button>
+            <input type="text" id="updateInput"></input>
+            <button>Alterar Nome</button>
+          </div>
+        })}
+
       </div>
     </div>
   );
--- server/index.js
+++ server/index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express();
 const mysql = require('mysql');
 
@@ -10,10 +12,55 @@ const db = mysql.createPool({
 });
 
 
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+app.get('/api/get', (err,res) =>{
+    const sqlSelect = "SELECT * FROM pontos_turisticos"
+    db.query(sqlSelect, (err, result) =>{
+        res.send(result);
+    })
+});
+
+app.post("/api/insert", (req,res) =>{
+
+    const nomeLugar = req.body.nomeLugar;
+    const descricaoLugar = req.body.descricaoLugar;
+    const enderecoLugar = req.body.enderecoLugar;
+    const estadoLugar = req.body.estadoLugar;
+    const cidadeLugar = req.body.cidadeLugar;
+
+    const sqlInsert = "INSERT INTO pontos_turisticos (nome, descricao, endereco, estado, cidade) VALUES (?,?,?,?,?)";
+    db.query(sqlInsert, [nomeLugar, descricaoLugar, enderecoLugar, estadoLugar, cidadeLugar], (err, result) =>{
+        console.log(result);
+    });
+});
 
 app.get('/', (req,res) =>{
     
 });
+
+app.delete('/api/delete/:nomeLugar', (req,res) =>{
+    const nomeLugar = req.params.nomeLugar;
+    console.log(nomeLugar)
+    const sqlDelete = "DELETE FROM pontos_turisticos WHERE nome = ?"
+    db.query(sqlDelete, nomeLugar, (err, result) =>{
+        if(err) console.log(err);
+    });
+})
+
+
+app.delete('/api/delete/:nomeLugar', (req,res) =>{
+    const nomeLugar = req.params.nomeLugar;
+    console.log(nomeLugar)
+    const sqlDelete = "DELETE FROM pontos_turisticos WHERE nome = ?"
+    db.query(sqlDelete, nomeLugar, (err, result) =>{
+        if(err) console.log(err);
+    });
+})
+
 
 app.listen(3001, () =>{
     console.log("Rodando");
