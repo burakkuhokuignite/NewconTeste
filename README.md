862b29a343c1d8b89b593ff2ae1d5db877c43650
burakkuhokuignite <matzsx@gmail.com>
2022-01-17 20:41:48 -0300

    melhoria no design e finalização de funções essenciais

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # NewconTeste
+
+O programa foi desenvolvido utilizando as seguintes tecnologias:
+Frontend: React
+Backend: NodeJs
+Banco de Dados: MySQL
+Editor: Visual Studio Code
+
+OBS: Eu tentei usar o .NET durante os dois primeiros dias do teste, mas o Visual Studio não parava de dar erro quando eu 
+tentava compilar o projeto, e eu ainda nem havia alterado nada, era só o projeto criado mesmo. Por isso, entrei em contato 
+com o Tadeu, que me passou o teste, e ele me disse que eu podia fazer em NodeJs mesmo. Durante minha fase de estudos para o teste,
+eu estudei sobre CRUD com React e Node, e por isso optei por fazer assim. 
+
+OBS2: Enquanto estudava, eu aprendi o conceito de Rotas em React, mas não tive tempo o bastante para aplicar isso no projeto, considerando
+que eu tive que fazer o projeto no último dia do teste, por isso a aplicação possui uma página apenas.
+
+
+
+Para compilar o projeto, cria-se dois terminais. Um abre a pasta 'client', e executa o comando 'npm start', e o outro abre a 
+pasta server, e executa o comando 'npm run devStart' (ou 'node index.js'), definido nos scripts do package.json. 
--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,10 @@
 import React, {useState, useEffect} from "react";
 import './App.css';
 import Axios from 'axios';
+import ReactDOM from 'react-dom';
+
+
+//import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
 function App() {
 
@@ -31,45 +35,62 @@ function App() {
   }
 
 
+  
+
+
   return (
-    <div className="App">
-      <h1>CRUD Pontos Turísticos</h1>
+          <div className="App">
+            <div className="screen">
+              <h1>CRUD Pontos Turísticos</h1>
+              <br></br><br></br>
 
-      <div className="form">
-        <label>Nome do Ponto Turístico:</label>
-        <input type="text" name="nomeLugar" onChange={(e) =>{
-          setNomeLugar(e.target.value);
-        }}></input>
-        <label>Descrição:</label>
-        <input type="text" name="descricaoLugar" onChange={(e) =>{
-          setDescricaoLugar(e.target.value);
-        }}></input>
-        <label>Endereço:</label>
-        <input type="text" name="enderecoLugar" onChange={(e) =>{
-          setEnderecoLugar(e.target.value);
-        }}></input>
-        <label>Estado:</label>
-        <input type="text" name="estadoLugar" onChange={(e) =>{
-          setEstadoLugar(e.target.value);
-        }}></input>
-        <label>Cidade:</label>
-        <input type="text" name="cidadeLugar" onChange={(e) =>{
-          setCidadeLugar(e.target.value);
-        }}></input>
+              <h2>Cadastro de Pontos Turísticos</h2>
 
-        <button onClick={submitLugar}>Cadastrar</button>
+              <div className="form">
+                <label>Nome do Ponto Turístico:</label>
+                <input type="text" name="nomeLugar" onChange={(e) =>{
+                  setNomeLugar(e.target.value);
+                }}></input>
+                <label>Descrição:</label>
+                <input type="text" name="descricaoLugar" onChange={(e) =>{
+                  setDescricaoLugar(e.target.value);
+                }}></input>
+                <label>Endereço:</label>
+                <input type="text" name="enderecoLugar" onChange={(e) =>{
+                  setEnderecoLugar(e.target.value);
+                }}></input>
+                <label>Estado:</label>
+                <input type="text" name="estadoLugar" onChange={(e) =>{
+                  setEstadoLugar(e.target.value);
+                }}></input>
+                <label>Cidade:</label>
+                <input type="text" name="cidadeLugar" onChange={(e) =>{
+                  setCidadeLugar(e.target.value);
+                }}></input>
 
-        {listaPontos.map((val) => {
-          return <div className="card"> <h1> {val.nome}  </h1> <p> Descrição: {val.descricao} </p> <p> Endereço: {val.endereco} </p> <p> Estado: {val.estado} </p> <p> Cidade: {val.cidade} </p>
-            <button onClick={() => {deleteLugar(val.nome)}}>Deletar</button>
-            <input type="text" id="updateInput"></input>
-            <button>Alterar Nome</button>
+                <button onClick={submitLugar}>Cadastrar</button>
+
+                <br></br><br></br>
+
+                <h2>Listagem de Pontos Turisticos</h2>
+
+                {listaPontos.map((val) => {
+                  return <div className="card"> <h1> {val.nome}  </h1> <p> Descrição: {val.descricao} </p> <p> Endereço: {val.endereco} </p> <p> Estado: {val.estado} </p> <p> Cidade: {val.cidade} </p>
+                    <button onClick={() => {deleteLugar(val.nome)}}>Deletar</button>
+                  </div>
+                })}
+
+                <div id="container1">
+                  
+                </div>
+
+              </div>
+            </div>
           </div>
-        })}
-
-      </div>
-    </div>
+        
   );
 }
+
+
 
 export default App;
